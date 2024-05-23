d15237dba9ab4fab8b1f562187cea793d8f1da93
myoksnt <santa.miyaoka@gmail.com>
2024-05-23 10:00:37 +0900

    contact

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ContactController::class, 'index']);
+Route::get('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::get('/contacts', [ContactController::class, 'store']);
+Route::post('/contacts', [ContactController::class, 'store']);
